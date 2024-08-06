a24b5267806c5f677352cb7606585829bce5175e
mayurnanwani <mayurnanwani17@gmail.com>
2024-08-06 12:17:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const form=document.querySelector('form');
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    const height=parseInt(document.querySelector('#height').value);
+    const weight=parseInt(document.querySelector('#weight').value);
+    const results=document.querySelector('#results');
+    if(height===''|| isNaN(height)||height<0){
+        results.innerHTML`Not valid`;
+    }
+    else if(weight===''|| isNaN(weight)||weight<0){
+        results.innerHTML`Not valid`;
+    }
+    else{
+        const bmi=(weight/((height*height)/10000)).toFixed(2);
+        results.innerHTML=`<span>${bmi}</span>`;
+    }    
+
+
+    
+
+})
